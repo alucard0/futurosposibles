e8961209b412a86aeaa82e33e2f5e36263871558
Amilkhael <amilkhael@outlook.com>
2018-03-13 11:23:22 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es-419">
+
+<head>
+
+  <!--Metas -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Strength in Diversity">
+    <meta name="author" content="La Salle México">
+
+
+    <!--Facebook -->
+
+    <title>Futuros Posibles | La Salle</title>
+
+    <!-- Bootstrap Core CSS -->
+    <!--<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Web Fonts -->
+    <script>WebFontConfig={google:{families: [ 'Roboto:100,300,400,500,700,900', 'Ultra']}}; (function(){var wf=document.createElement('script'); wf.src=('https:'==document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js'; wf.type='text/javascript'; wf.async='true'; var s=document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wf, s);})(); 
+    </script>
+
+    <!-- Plugin CSS -->
+
+
+    <!-- Theme CSS -->
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="shortcut icon" href="images/favicon/favicon.ico" type="image/x-icon">
+</head>
+
+<body >
+
+<section class="container-fluid">
+  
+</section>
+
+<?php include 'footer.php';?>
